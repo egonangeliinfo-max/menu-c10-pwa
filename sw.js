89618aba57c8794f217fb79b84225b9a044d744e
egonangeliinfo-max <egon.angeli.info@gmail.com>
2026-01-15 15:26:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,21 @@
 self.addEventListener("push", (event) => {
-  const data = event.data ? event.data.json() : {};
+  let data = {};
+  try {
+    data = event.data ? event.data.json() : {};
+  } catch (e) {
+    data = { title: "Menu scuola", body: event.data ? event.data.text() : "" };
+  }
+
   const title = data.title || "Menu scuola";
   const body = data.body || "";
 
   event.waitUntil(
     self.registration.showNotification(title, {
       body,
+      icon: data.icon || undefined,
+      badge: data.badge || undefined,
+      tag: "menu-c10",
+      renotify: true,
       data: { url: data.url || "./" }
     })
   );
